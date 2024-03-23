3a71a5c2ce98b1be6a9582d65eef2cc2465df234
Stepan Fedorov <StFedorov@gmail.com>
2024-03-22 21:50:23 -0600

    Fix: TF TG tokens typo

--- deploy/02-gcp-functions/01.secrets.tf
+++ deploy/02-gcp-functions/01.secrets.tf
@@ -34,7 +34,7 @@ resource "google_secret_manager_secret_version" "bot_cred_tg_token_version" {
 
   count = length(local.bots_secrets_list)
 
-  secret = google_secret_manager_secret.bot_cred_tg_secret[count.index].id
+  secret = google_secret_manager_secret.bot_cred_tg_token[count.index].id
 
   secret_data = local.bots_secrets_list[count.index].token
 }
