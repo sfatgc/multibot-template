90dee01479d01a2d9d0bc4c8080a12f4dc674976
Stepan Fedorov <StFedorov@gmail.com>
2024-03-23 14:43:38 -0600

    Fix: duplicates

--- deploy/02-gcp-functions/00.data-gcp.tf
+++ deploy/02-gcp-functions/00.data-gcp.tf
@@ -1,9 +1,2 @@
 data google_client_config "this" {}
 data google_project "project" {}
-
-locals {    
-  region      = data.google_client_config.this.region
-  project     = data.google_client_config.this.project
-  project_num = data.google_project.project.number
-  project_id  = data.google_project.project.id
-}
