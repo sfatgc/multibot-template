fd1dc8fc6e9dd9310ce74dc94929a0b71d137045
Stepan Fedorov <StFedorov@gmail.com>
2024-03-24 14:22:55 -0600

    Upd: add logging, set bot verbosity

--- functions/multibot/bot.go
+++ functions/multibot/bot.go
@@ -1,6 +1,7 @@
 package multibot
 
 import (
+	"log"
 	"reflect"
 
 	"gopkg.in/telebot.v3"
@@ -15,7 +16,7 @@ type TgBot struct {
 
 func NewBot(bot_name string, bot_telegram_token string, bot_webhook_secret string, bot_webhook_url string) (*TgBot, error) {
 
-	tbs := telebot.Settings{Token: bot_telegram_token}
+	tbs := telebot.Settings{Token: bot_telegram_token, Verbose: true}
 
 	tele_bot, err := telebot.NewBot(tbs)
 	if err != nil {
@@ -47,29 +48,55 @@ func NewBot(bot_name string, bot_telegram_token string, bot_webhook_secret strin
 }
 
 func (b *TgBot) CheckWebhook() bool {
+
+	result := true
+
 	tb := b.TgBot
 	wh, err := tb.Webhook()
 
 	if err == nil {
 
+		log.Printf("Webhook check: Bot %s webhook exists", b.BotName)
+
 		if wh.Endpoint == nil || b.TgWebhook.Endpoint == nil {
-			return false
-		}
-		if wh.Endpoint.PublicURL != b.TgWebhook.Endpoint.PublicURL {
-			return false
-		}
-		if wh.SecretToken != b.TgWebhook.SecretToken {
-			return false
-		}
-		if wh.MaxConnections != b.TgWebhook.MaxConnections {
-			return false
-		}
-		if !reflect.DeepEqual(wh.AllowedUpdates, b.TgWebhook.AllowedUpdates) {
-			return false
+			result = false
+		} else if wh.Endpoint.PublicURL != b.TgWebhook.Endpoint.PublicURL {
+			log.Printf("Webhook check: Bot %s webhook URL (%s) doesn't match desired value (%s)",
+				b.BotName,
+				wh.Endpoint.PublicURL,
+				b.TgWebhook.Endpoint.PublicURL)
+			result = false
 		}
 
-		return true
+		// TODO: Remove secret value logging after debug
+		if wh.SecretToken != b.TgWebhook.SecretToken {
+			log.Printf("Webhook check: Bot %s webhook secret (%s) doesn't match desired value (%s)",
+				b.BotName,
+				wh.SecretToken,
+				b.TgWebhook.SecretToken)
+			result = false
+		}
+
+		if wh.MaxConnections != b.TgWebhook.MaxConnections {
+			log.Printf("Webhook check: Bot %s webhook MaxConnections (%d) doesn't match desired value (%d)",
+				b.BotName,
+				wh.MaxConnections,
+				b.TgWebhook.MaxConnections)
+			result = false
+		}
+
+		if !reflect.DeepEqual(wh.AllowedUpdates, b.TgWebhook.AllowedUpdates) {
+			log.Printf("Webhook check: Bot %s webhook AllowedUpdates (%v) doesn't match desired value (%v)",
+				b.BotName,
+				wh.AllowedUpdates,
+				b.TgWebhook.AllowedUpdates)
+			result = false
+		}
+
+	} else {
+		log.Printf("Webhook check: Bot %s webhook is not set", b.BotName)
+		result = false
 	}
 
-	return false
+	return result
 }
